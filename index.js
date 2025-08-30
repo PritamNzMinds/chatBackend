e2bddb3506162636625e9b51ad6d8820a210b74e
PritamNzMinds <pritam.adhikari@nzminds.com>
2025-08-30 16:11:28 +0530

    socket fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import dbCon from "./app/config/dbConnection.js";
 import router from "./app/router/index.js";
 import { Message } from "./app/model/message.model.js";
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors());
 
 // call router
-app.use(router)
+app.use(router);
 
 const io = new Server(server, {
   cors: { origin: "*" },
@@ -31,40 +31,61 @@ let users = [];
 io.on("connection", (socket) => {
   console.log("New client connected:", socket.id);
 
-  // add user
-  // socket.on("adduser", (data) => {
-  //   const user = { ...data, socketId: socket.id };
-  //   users.push(user);
-  //   io.emit("getUsers", users);
-  // });
-  // io.emit("getUsers", users);
 
-  socket.on("addUser", (userId) => {
-    socket.join(userId);
-    console.log(`User ${userId} joined their room`);
+  // add user
+  socket.on("addUser", (data) => {
+    console.log("data", data);
+    if (!users.some((u) => u.id.toString() === data.id.toString())) {
+      users.push({ ...data, socketId: socket.id });
+    }
+    io.emit("getUsers", users);
+    console.log("users", users);
   });
 
+  // socket.on("addUser", (userId) => {
+  //   socket.join(userId);
+  //   console.log(`User ${userId} joined their room`);
+  // });
+
   // send message to specific user
+  // socket.on("sendMessage", async (data) => {
+
+  //   const {senderId,message,receiverId}=data;
+  //   // save data in db
+  //   const newMessage=new Message({senderId,message,receiverId});
+  //   const newMsg=await newMessage.save();
+
+  //   const users=await User.find({});
+  //   const receiver = users.find((u) => u._id.toString() === receiverId);
+  //   console.log(newMsg);
+  //   if (receiver) {
+  //     console.log(receiver);
+  //     console.log("receiver",receiver);
+  //     io.to(receiver._id.toString()).emit("getMessage",newMsg);
+  //   }
+  // });
   socket.on("sendMessage", async (data) => {
+    const { senderId, message, receiverId } = data;
 
-    const {senderId,message,receiverId}=data;
-    // save data in db
-    const newMessage=new Message({senderId,message,receiverId});
-    const newMsg=await newMessage.save();
-
-    const users=await User.find({});
-    const receiver = users.find((u) => u._id.toString() === receiverId);
-    console.log(newMsg);
+    // save message to DB
+    const newMessage = new Message({ senderId, message, receiverId });
+    const newMsg = await newMessage.save();
+    // console.log(newMsg);
+    // find receiver from active users (not DB)
+    console.log("receiver id", receiverId);
+    const receiver = users.find((u) => u.id.toString() === receiverId);
     if (receiver) {
-      io.to(receiver._id.toString()).emit("getMessage",newMsg);
+      // io.to(receiver.socketId).emit("getMessage", newMsg);
+      io.to(receiver.socketId).emit("getMessage", newMsg);
+    } else {
+      console.log("Receiver not found in  users list");
     }
   });
 
   // remove user on disconnect
   socket.on("disconnect", () => {
     users = users.filter((u) => u.socketId !== socket.id);
-    console.log("User disconnected:", socket.id);
-    io.emit("getUsers", users); 
+    io.emit("getUsers", users);
   });
 });
 
